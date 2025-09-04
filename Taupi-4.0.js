6362c712567dfc349dd3765c8e6774095387f5dd
Bob <187332127+BoeserBob@users.noreply.github.com>
2025-09-04 20:51:57 +0200

    Update Taupi-4.0.js

--- Taupi-4.0.js
+++ Taupi-4.0.js
@@ -141,7 +141,7 @@ function checkBlu(event) {
 Timer.set(schaltzeit * 1000, true, function () {
   print("----- Steuerung alle", schaltzeit, "s -----");
   print("Innen: T =", temperatur_innen, "°C, RH =", humidity_innen, "%, Tp =", taupunkt_innen, "Batterie: ", battery_innen " % ");
-  print("Außen: T =", temperatur_aussen, "°C, RH =", humidity_aussen, "%, Tp =", taupunkt_aussen, "Batterie: ", battery_aussen " % "));
+  print("Außen: T =", temperatur_aussen, "°C, RH =", humidity_aussen, "%, Tp =", taupunkt_aussen, "Batterie: ", battery_aussen " % ");
   schalten();
 });
 
